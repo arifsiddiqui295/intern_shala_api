1cdc00314938fbccb2dc69b1d309b6480d51a90b
arifsiddiqui295 <arifsiddiqui2905@gmail.com>
2024-03-21 01:01:33 +0530

    error handeling

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require("express");
 const app = express();
 //logger
 const logger = require('morgan');
+const { generatedErros } = require("./middlewares/error");
+const ErrorHandler = require("./utils/errorHandler");
 app.use(logger("dev"))
 //routes
 app.use("/",require("./routes/indexRoutes"))
-
+//error handeling
+app.all("*",(req,res,next) => {
+    next(new ErrorHandler(`Request Url not found: ${req.url}`,404))
+})
+app.use(generatedErros)
 app.listen(process.env.PORT, (error) => {
     if (error) {
         console.error("Error starting server:", error);
